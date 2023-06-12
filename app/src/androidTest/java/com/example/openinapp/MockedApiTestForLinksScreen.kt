410f73387b4086992a3045baab13feb66756af2c
arsalanahmad4 <arsalanahmad0407@gmail.com>
2023-06-12 20:33:48 +0530

    simpleTestCase

--- app/src/androidTest/java/com/example/openinapp/MockedApiTestForLinksScreen.kt
+++ app/src/androidTest/java/com/example/openinapp/MockedApiTestForLinksScreen.kt
@@ -1,7 +1,12 @@
 package com.example.openinapp
 
 import android.os.SystemClock
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.openinapp.testutils.AssetReaderUtil
 import com.example.openinapp.testutils.MockedAPITest
@@ -37,6 +42,11 @@ class MockedApiTestForLinksScreen: MockedAPITest() {
         )
         SystemClock.sleep(1000)
         val scenario = launchActivity<DashboardActivity>()
+        SystemClock.sleep(1000)
+
+        Espresso.onView(withId(R.id.tvGreetings))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        scenario.close()
         SystemClock.sleep(1000)
     }
 }
